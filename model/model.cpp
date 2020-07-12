a9fd74ce7c1d7e5be92e2f71dc8710afd055cd0e
lizhi <lizhiwj2019@outlook.com>
2020-07-12 15:15:29 +0800

    增加注释，简单修改ui不涉及代码

--- model/model.cpp
+++ model/model.cpp
@@ -1,5 +1,5 @@
 #include"model.h"
-
+#include<QMessageBox>
 Model::Model()
 {
     //给数据成员分配空间
@@ -25,16 +25,31 @@ std::shared_ptr<std::string> Model::get_Htmltxt()
 //此处只是为了便于编译通过
 bool Model::downLoad()
 {
-    //do somthing
+
+    //无法保证url和dir合法，先进行简单检查
+    if(*_url==""||*_dir=="")
+    {
+        return false;
+    };
+
+    //do somthing about download
+    //最好另开线程进行下载，否则下载时间太长时，整个程序会因等待下载而失去响应
+    //不过第一轮迭代不要求这一点，之后再添加
+    //此处不进行实际保存，存到变量_htmltxt中即可
+
+
+
+    //最后弹出消息窗作为测试
+    QMessageBox::information(nullptr,"download","succeed");
     return true;
-};//
+};
 bool Model::setUrl(const std::string& str)
 {
-    //do somthing
+    *_url=str;
     return true;
 };
 bool Model::setDir(const std::string& str)
 {
-    //do somthing
+    *_dir=str;
     return true;
 };
