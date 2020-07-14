1ebca877b24e25c07d1dc4a3f0357b1c9a1dcbc6
Onlynagesha <orina_zju@163.com>
2020-07-14 17:38:14 +0800

    实现任务列表

--- common/def.h
+++ common/def.h
@@ -1,19 +1,29 @@
 #pragma once
-#ifndef DEF_H
-#define DEF_H
-
-#endif // DEF_H
 
 //定义文件类型
 #define TYPE_HTML 100
 #define TYPE_TEXT 101
 #define TYPE_PHOTO 102
 
-
 //定义属性改变的消息
 #define TASK_LIST_CHANGED 1000
 
 #include<string>
+
+inline std::string fileTypeIdToString(int type)
+{
+    switch (type) {
+    case TYPE_HTML:
+        return "HTML代码";
+    case TYPE_TEXT:
+        return "文本";
+    case TYPE_PHOTO:
+        return "图片";
+    default:
+        return "ERROR: invalid type ID";
+    }
+}
+
 struct WebTask
 {
     int id;
