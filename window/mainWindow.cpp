30a479a9de21ff5165743aecba73f61fef0965bc
shooterhex <1937891400@qq.com>
2020-07-24 10:17:23 +0800

    Bugfix #1

--- window/mainWindow.cpp
+++ window/mainWindow.cpp
@@ -92,7 +92,7 @@ void MainWindow::onDownloadButtonPressed()
     }
     m_viewModel->get_SetTypeCommand()(typeID);
 
-    auto res = m_cmdFunc_Download(std::any(WebTask{.id = 0, .url = url, .dir = dir, .type = typeID}));
+    auto res = m_cmdFunc_Download(std::any(WebTask{0, url, dir, typeID}));
 }
 
 void MainWindow::onChooseFileButtonPressed()
